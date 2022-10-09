bbfdac7a3b9d9285136be5c11d1de2ea2b7cd75d
c4ei <c4ei.net@gmail.com>
2022-10-10 04:55:17 +0900

    busd

--- packages/uikit/src/components/Image/tokens.ts
+++ packages/uikit/src/components/Image/tokens.ts
@@ -60,7 +60,7 @@ const tokens = {
       56: "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56",
       97: "",
       21004: "0xBa9303709c2Ad10b62a598e18e15b1E42921edea",
-      8217: "0x1d43F7539C9764502A054b83477b4dbeA6F7e51D", //(22-10-10)
+      8217: "0x8Eed4BFE1Bba9ee60c3E4A6AFD2bDD8102e8590C", //(22-10-10)
     },
     decimals: 18,
     projectLink: "https://www.paxos.com/busd/",
