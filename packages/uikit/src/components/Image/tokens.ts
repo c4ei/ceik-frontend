53bd7e459ca89793e2469192ecbfbaddfb548076
c4ei <c4ei.net@gmail.com>
2022-10-12 22:37:06 +0900

    8217: alba , cake

--- packages/uikit/src/components/Image/tokens.ts
+++ packages/uikit/src/components/Image/tokens.ts
@@ -7,8 +7,8 @@ const tokens = {
     address: {
       56: "0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82",
       97: "0xFa60D973F7642B748046464e165A65B7323b0DEE",
-      21004: "0x1A10F46a2902078d8a1edB576FdAF8CB9B125d69", //(22-09-27)
       8217: "0x4A01A7ecE21199b20e4A5B48c3C1D95f95d98788", //(22-10-10)
+      21004: "0x1A10F46a2902078d8a1edB576FdAF8CB9B125d69", //(22-09-27)
     },
     decimals: 18,
     projectLink: "https://ceik.c4ei.net/",
@@ -28,8 +28,8 @@ const tokens = {
     address: {
       56: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
       97: "0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd",
-      21004: "0x83f7AaE73a80e5Ae7Fa4a4948F1CE90f57c638aa",
       8217: "0xAb57000f39107eDE71441D54Aa53A22bdCE9F4B9", //(22-10-10)
+      21004: "0x83f7AaE73a80e5Ae7Fa4a4948F1CE90f57c638aa",
     },
     decimals: 18,
     projectLink: "https://ceik.c4ei.net/",
@@ -49,6 +49,7 @@ const tokens = {
     address: {
       56: "0x009cF7bC57584b7998236eff51b98A168DceA9B0",
       97: "0xfE1e507CeB712BDe086f3579d2c03248b2dB77f9",
+      8217: "0x2041dAA87620e055496eBF4C4e7aBBFd8280795d", //(22-10-10)
       21004: "0xABB8a013F21a491C2e35d143A31FB708499A92c0",
     },
     decimals: 18,
@@ -59,8 +60,8 @@ const tokens = {
     address: {
       56: "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56",
       97: "",
-      21004: "0xBa9303709c2Ad10b62a598e18e15b1E42921edea",
       8217: "0x8Eed4BFE1Bba9ee60c3E4A6AFD2bDD8102e8590C", //(22-10-10)
+      21004: "0xBa9303709c2Ad10b62a598e18e15b1E42921edea",
     },
     decimals: 18,
     projectLink: "https://www.paxos.com/busd/",
@@ -70,6 +71,7 @@ const tokens = {
     address: {
       56: "0x2170Ed0880ac9A755fd29B2688956BD959F933F8",
       97: "",
+      8217: "",
       21004: "",
     },
     decimals: 18,
@@ -80,6 +82,7 @@ const tokens = {
     address: {
       56: "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d",
       97: "0xaB1a4d4f1D656d2450692D237fdD6C7f9146e814",
+      8217: "",
       21004: "",
     },
     decimals: 18,
@@ -110,6 +113,7 @@ const tokens = {
     address: {
       56: "0x55d398326f99059fF775485246999027B3197955",
       97: "0xE02dF9e3e622DeBdD69fb838bB799E3F168902c5",
+      8217: "",
       21004: "",
     },
     decimals: 18,
